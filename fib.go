b8eb48f9c70412411f3cbb98327062f0354bb519
yushengguo557 <yushengguo557@gmail.com>
2023-03-02 12:19:28 +0800

    😃

---
+++ fib.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// 方法三 通项公式
+func fib(n int) int {
+	sqrt5 := math.Sqrt(5)
+	p1 := math.Pow((1+sqrt5)/2, float64(n))
+	p2 := math.Pow((1-sqrt5)/2, float64(n))
+	return int(math.Round((p1 - p2) / sqrt5))
+}
+
+// 方法二 迭代
+//func fib(n int) int {
+//	if n < 2 {
+//		return n
+//	}
+//	a, b := 0, 1
+//	var ret int
+//	for i := 1; i < n; i++ {
+//		ret = b + a
+//		b, a = ret, b
+//	}
+//	return ret
+//}
+
+// 方法一 递归
+//func fib(n int) int {
+//	if n < 2 {
+//		return n
+//	}
+//	return fib(n-1) + fib(n-2)
+//}
+
+// 1 1 2 3 5
+func main() {
+	fmt.Println(fib(5))
+}
---
+++ printBin.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// 面试题 05.02. 二进制数转字符串
+// https://leetcode.cn/problems/bianry-number-to-string-lcci/
+
+func printBin(num float64) string {
+	ret := []rune{'0', '.'}
+	count := 2 // 计数器
+	for count < 32 && num != 0 {
+		ret = append(ret, rune(num*2)+'0')
+		num = num*2 - float64(int(num*2))
+		count++
+	}
+	if count < 32 {
+		return string(ret)
+	}
+	return "ERROR"
+}
+
+func main() {
+	ret := printBin(0.625)
+	fmt.Println(ret)
+}
