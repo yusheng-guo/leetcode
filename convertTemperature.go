404d2ac7215d2e1101499f0764f1d498051566be
unknown <yushengguo557@gmail.com>
2023-03-21 23:33:07 +0800

    😗

---
+++ convertTemperature.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// 2469. 温度转换
+// https://leetcode.cn/problems/convert-the-temperature/
+func convertTemperature(celsius float64) []float64 {
+	kelvin := celsius + 273.15
+	fahrenheit := celsius*1.80 + 32.00
+	return []float64{kelvin, fahrenheit}
+}
+
+func main() {
+	celsius := 36.50
+	ret := convertTemperature(celsius)
+	fmt.Println(ret)
+}
