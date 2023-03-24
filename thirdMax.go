5f8d8cf3907a92f1d2879665751ee3441cf264d5
unknown <yushengguo557@gmail.com>
2023-03-25 00:21:15 +0800

    😊

--- thirdMax.go
+++ thirdMax.go
@@ -7,32 +7,59 @@ import (
 
 // 414. 第三大的数
 // https://leetcode.cn/problems/third-maximum-number/
+// 方法三 方法二优化 一次循环
 func thirdMax(nums []int) int {
-	max(nums)
-}
-
-var count = 0
-var thirdMaxNum = 0
-
-func max(nums []int) (int, int) {
-	m := math.MinInt
-	index := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] > m {
-			m = nums[i]
-			index = i
+	firstMaxNum, secondMaxNum, thirdMaxNum := math.MinInt, math.MinInt, math.MinInt
+	for j := 0; j < len(nums); j++ {
+		if nums[j] > firstMaxNum {
+			thirdMaxNum = secondMaxNum
+			secondMaxNum = firstMaxNum
+			firstMaxNum = nums[j]
+		}
+		if nums[j] < firstMaxNum && nums[j] > secondMaxNum {
+			thirdMaxNum = secondMaxNum
+			secondMaxNum = nums[j]
+		}
+		if nums[j] < secondMaxNum && nums[j] > thirdMaxNum {
+			thirdMaxNum = nums[j]
 		}
 	}
-	if index == len(nums)-1 {
-		nums = nums[:index]
+	if thirdMaxNum == math.MinInt {
+		return firstMaxNum
 	}
-	nums = append(nums[:index], nums[index+1:]...)
-	count++
-	if count > 2 {
-		thirdMaxNum = m
-	}
-	return max(nums)
+	return thirdMaxNum
 }
+
+// 方法二 三次循环
+//func thirdMax(nums []int) int {
+//	firstMaxNum, secondMaxNum, thirdMaxNum := math.MinInt, math.MinInt, math.MinInt
+//	for j := 0; j < len(nums); j++ {
+//		if nums[j] > firstMaxNum {
+//			firstMaxNum = nums[j]
+//		}
+//	}
+//	for j := 0; j < len(nums); j++ {
+//		if nums[j] == firstMaxNum {
+//			continue
+//		}
+//		if nums[j] > secondMaxNum {
+//			secondMaxNum = nums[j]
+//		}
+//	}
+//
+//	for j := 0; j < len(nums); j++ {
+//		if nums[j] == firstMaxNum || nums[j] == secondMaxNum {
+//			continue
+//		}
+//		if nums[j] > thirdMaxNum {
+//			thirdMaxNum = nums[j]
+//		}
+//	}
+//	if thirdMaxNum == math.MinInt {
+//		return firstMaxNum
+//	}
+//	return thirdMaxNum
+//}
 
 // 方法一：排序+查找
 //func thirdMax(nums []int) int {
