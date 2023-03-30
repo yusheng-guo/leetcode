5629ac7b03d5f8d2b7ae17cfd91af5ddb9d937ef
unknown <yushengguo557@gmail.com>
2023-03-30 23:06:15 +0800

    😗

---
+++ temperatureTrend.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+// LCP 61. 气温变化趋势
+// https://leetcode.cn/problems/6CE719/?envType=study-plan&id=2022-qiu-sai-ti-ji&plan=lccup&plan_progress=x5979eah
+//const (
+//	up = iota
+//	intact
+//	down
+//)
+
+// 方法二 简化
+func temperatureTrend(temperatureA []int, temperatureB []int) int {
+	l := len(temperatureA)
+	ret := 0
+	var rets []int
+	for i := 1; i < l; i++ {
+		if (temperatureA[i] > temperatureA[i-1] && temperatureB[i] > temperatureB[i-1]) ||
+			(temperatureA[i] == temperatureA[i-1] && temperatureB[i] == temperatureB[i-1]) ||
+			(temperatureA[i] < temperatureA[i-1] && temperatureB[i] < temperatureB[i-1]) {
+			ret++
+			continue
+		}
+		rets = append(rets, ret)
+		ret = 0
+	}
+	for i := 0; i < len(rets); i++ {
+		if ret < rets[i] {
+			ret = rets[i]
+		}
+	}
+	return ret
+}
+
+// 方法一
+//func temperatureTrend(temperatureA []int, temperatureB []int) int {
+//	l := len(temperatureA)
+//	ret := 0
+//	var rets []int
+//	for i := 1; i < l; i++ {
+//		if temperatureA[i] > temperatureA[i-1] && temperatureB[i] > temperatureB[i-1] {
+//			ret++
+//			continue
+//		}
+//		if temperatureA[i] == temperatureA[i-1] && temperatureB[i] == temperatureB[i-1] {
+//			ret++
+//			continue
+//		}
+//		if temperatureA[i] < temperatureA[i-1] && temperatureB[i] < temperatureB[i-1] {
+//			ret++
+//			continue
+//		}
+//		rets = append(rets, ret)
+//		ret = 0
+//	}
+//	for i := 0; i < len(rets); i++ {
+//		if ret < rets[i] {
+//			ret = rets[i]
+//		}
+//	}
+//	return ret
+//}
+
+func main() {
+	temperatureA := []int{21, 18, 18, 18, 31}
+	temperatureB := []int{34, 32, 16, 16, 17}
+	ret := temperatureTrend(temperatureA, temperatureB)
+	fmt.Println(ret)
+}
---
+++ transportationHub.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func transportationHub(path [][]int) int {
+	l := len(path)
+	out := map[int]struct{}{}      // 出
+	in := map[int]int{}            // 入
+	location := map[int]struct{}{} // 所有地点
+	for i := 0; i < l; i++ {
+		if _, ok := out[path[i][0]]; !ok {
+			out[path[i][0]] = struct{}{}
+		}
+		if _, ok := location[path[i][0]]; !ok {
+			location[path[i][0]] = struct{}{}
+		}
+		if _, ok := location[path[i][1]]; !ok {
+			location[path[i][1]] = struct{}{}
+		}
+		in[path[i][1]]++
+	}
+	fmt.Println(out, in, location)
+	for k, v := range in {
+		_, ok := out[k]
+		if v == len(location)-1 && !ok {
+			return k
+		}
+	}
+	return -1
+}
+
+func main() {
+	//path := [][]int{{0, 1}, {0, 3}, {1, 3}, {2, 0}, {2, 3}}
+	path := [][]int{{1, 3}, {1, 2}, {0, 2}, {3, 2}}
+	ret := transportationHub(path)
+	fmt.Println(ret)
+}
