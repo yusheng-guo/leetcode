ad7195aed2410fce520b97c8249245f07a4438c3
unknown <yushengguo557@gmail.com>
2023-04-03 13:35:44 +0800

    😬

---
+++ prevPermOpt1.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// 1053. 交换一次的先前排列
+// https://leetcode.cn/problems/previous-permutation-with-one-swap/
+// 贪心算法
+func prevPermOpt1(arr []int) []int {
+	// 找i
+	for i := len(arr) - 2; i >= 0; i-- {
+		if arr[i] > arr[i+1] {
+			// 找j
+			j := len(arr) - 1
+			for arr[j] >= arr[i] || arr[j] == arr[j-1] {
+				j--
+			}
+			arr[i], arr[j] = arr[j], arr[i]
+			break
+		}
+	}
+	return arr
+}
+
+func main() {
+	arr := []int{3, 1, 1, 3}
+	ret := prevPermOpt1(arr)
+	fmt.Println(ret)
+}
