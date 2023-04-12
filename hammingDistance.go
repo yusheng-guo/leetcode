a0703f1892ee40dfc348eddb5b1b07de4c5dc90d
unknown <yushengguo557@gmail.com>
2023-04-12 23:43:47 +0800

    🥰

---
+++ hammingDistance.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 461. 汉明距离
+// https://leetcode.cn/problems/hamming-distance/
+// 方法四 Brian Kernighan 算法
+func hammingDistance(x int, y int) int {
+	count := 0
+	ret := x ^ y
+	for ret != 0 {
+		count++
+		ret &= ret - 1
+	}
+	return count
+}
+
+// 方法三 异或 + 计数
+//func hammingDistance(x int, y int) int {
+//	count := 0
+//	ret := x ^ y
+//	for ret != 0 {
+//		if ret&1 == 1 {
+//			count++
+//		}
+//		ret >>= 1
+//	}
+//	return count
+//}
+
+// 方法二 内置函数
+//func hammingDistance(x int, y int) int {
+//	return bits.OnesCount(uint(x ^ y))
+//}
+
+// 方法一 移位
+//func hammingDistance(x int, y int) int {
+//	distance := 0
+//	for x != 0 || y != 0 {
+//		if x&1 != y&1 {
+//			distance++
+//		}
+//		x >>= 1
+//		y >>= 1
+//	}
+//	return distance
+//}
+
+func main() {
+	x := 1
+	y := 4
+	ret := hammingDistance(x, y)
+	fmt.Println(ret)
+}
---
+++ hammingWeight.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// 191. 位1的个数
+// https://leetcode.cn/problems/number-of-1-bits/description/
+// Brian Kernighan 算法
+func hammingWeight(num uint32) (count int) {
+	for num != 0 {
+		count++
+		num &= num - 1
+	}
+	return count
+}
+
+func main() {
+	ret := hammingWeight(11)
+	fmt.Println(ret)
+}
