cd51aaa9e4ce6c2fa4d208d086cc4f9194d275bb
yushengguo557 <yushengguo557@gmail.com>
2023-06-09 23:58:41 +0800

    modifiedGraphEdges

---
+++ modifiedGraphEdges.py
@@ -0,0 +1,43 @@
+class Solution:
+    def modifiedGraphEdges(self, n: int, edges: List[List[int]], source: int, destination: int, target: int) -> List[List[int]]:
+        def dijkstra(op: int, source: int, adj_matrix: List[List[int]]) -> List[int]:
+            # 朴素的 dijkstra 算法
+            # adj_matrix 是一个邻接矩阵
+            dist = [float("inf")] * n
+            used = set()
+            dist[source] = 0
+
+            for round in range(n - 1):
+                u = -1
+                for i in range(n):
+                    if i not in used and (u == -1 or dist[i] < dist[u]):
+                        u = i
+                used.add(u)
+                for v in range(n):
+                    if v not in used and adj_matrix[u][v] != -1:
+                        if edges[adj_matrix[u][v]][2] != -1:
+                            dist[v] = min(dist[v], dist[u] + edges[adj_matrix[u][v]][2])
+                        else:
+                            if op == 0:
+                                dist[v] = min(dist[v], dist[u] + 1)
+                            else:
+                                modify = target - dist[u] - from_destination[v]
+                                if modify > 0:
+                                    dist[v] = min(dist[v], dist[u] + modify)
+                                    edges[adj_matrix[u][v]][2] = modify
+                                else:
+                                    edges[adj_matrix[u][v]][2] = target
+            return dist
+
+        adj_matrix = [[-1] * n for _ in range(n)]
+        # 邻接矩阵中存储边的下标
+        for i, (u, v, w) in enumerate(edges):
+            adj_matrix[u][v] = adj_matrix[v][u] = i
+        
+        from_destination = dijkstra(0, destination, adj_matrix)
+        if from_destination[source] > target:
+            return []
+        from_source = dijkstra(1, source, adj_matrix)
+        if from_source[destination] != target:
+            return []
+        return edges
