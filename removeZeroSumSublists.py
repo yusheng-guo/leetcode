bba1ac6105dfbe614894e989d89155d4da9dcd15
yushengguo557 <yushengguo557@gmail.com>
2023-06-11 23:54:47 +0800

    removeZeroSumSublists

---
+++ removeZeroSumSublists.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prefix = 0
+        seen = {}
+        seen[0] = dummy = ListNode(0)
+        dummy.next = head
+        while head:
+            prefix += head.val
+            seen[prefix] = head
+            head = head.next
+        head = dummy
+        prefix = 0
+        while head:
+            prefix += head.val
+            head.next = seen[prefix].next
+            head = head.next
+        return dummy.next
