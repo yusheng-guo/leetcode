e6e154128001703fe11d9a5d7d98d1c590efa68e
yushengguo557 <yushengguo557@gmail.com>
2023-07-08 23:33:27 +0800

    twoSum

---
+++ twoSum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        low, high = 0, len(numbers) - 1
+        while low < high:
+            total = numbers[low] + numbers[high]
+            if total == target:
+                return [low + 1, high + 1]
+            elif total < target:
+                low += 1
+            else:
+                high -= 1
+
+        return [-1, -1]
