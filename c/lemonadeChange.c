271e4f1b54e301576a2bc4dbde80eccdc26c5404
yushengguo557 <yushengguo557@gmail.com>
2023-07-22 23:13:48 +0800

    lemonadeChange

---
+++ c/lemonadeChange.c
@@ -0,0 +1,24 @@
+bool lemonadeChange(int* bills, int billsSize) {
+    int five = 0, ten = 0;
+    for (int i = 0; i < billsSize; i++) {
+        if (bills[i] == 5) {
+            five++;
+        } else if (bills[i] == 10) {
+            if (five == 0) {
+                return false;
+            }
+            five--;
+            ten++;
+        } else {
+            if (five > 0 && ten > 0) {
+                five--;
+                ten--;
+            } else if (five >= 3) {
+                five -= 3;
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;
+}
